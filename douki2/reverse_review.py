47aaa0a00c46c61f367d9b4021b6b802da85f609
YuanzhongLi <32239538+YuanzhongLi@users.noreply.github.com>
2018-01-12 10:03:24 +0900

    Update reverse_review.py

--- douki2/reverse_review.py
+++ douki2/reverse_review.py
@@ -1,22 +1,25 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 # jsonを扱う
 import json
 #Pandas numpyをインポート
 import pandas as pd
 import numpy as np
 
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from tinydb import TinyDB, Query
 review_db = TinyDB('review.json')
 user_db = TinyDB('user.json')
 @app.route("/")
+@app.route("/register")
 def register():
     return render_template('account_register.html')
-
-@app.route("/account_register")
-def account_2():
-    return render_template('home.html')
 
 @app.route("/home")
 def home():
@@ -111,7 +114,7 @@ def add():
     })
 
 
-    return information()
+    return 
 
 @app.route("/account_register")
 def account_register():
@@ -137,8 +140,33 @@ def reset():
         review_db.purge()
     review_db.insert({'user': 'A','total_point':3,"attendance_point":4,"credit_point":5,"score_point":4, "review_comment":"可愛い過ぎる"})
     if user_db is not None:
-        review_db.purge()
+        user_db.purge()
+        
     return register()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
 
 if __name__ == "__main__":
     app.run(debug = True)
